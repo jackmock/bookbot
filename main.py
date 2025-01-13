45270942bdf37df431bc15136fccf0aeee888133
jackmock <jack.r.mock@gmail.com>
2025-01-12 20:49:57 -0600

    added report function, sorted characters by frequency.

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
-    print(f"There are {num_words} words in this document.")
     num_char = char_count(text)
-    print(num_char)
+    char_report(book_path, num_words, num_char)
 
 def get_book_text(path):
       with open(path) as f:
@@ -24,5 +23,18 @@ def char_count(input):
             char_dict[char] = 1
     return char_dict
 
+def char_report(path, wordcount, input):
+    alpha_only = {}
+    for char in input:
+        if char.isalpha():
+            alpha_only[char] = input[char]
+    alpha_only_sorted = sorted(alpha_only.items(), key=lambda item: item[1], reverse=True)
+
+    
+    print(f"--- Begin report of {path} ---")
+    print(f"There are {wordcount} words in this document \n")
+    for char, count in alpha_only_sorted:
+        print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
 
 main()
